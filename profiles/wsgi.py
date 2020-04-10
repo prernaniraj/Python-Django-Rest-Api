3c5cfac08c24f9657c0be14c6e182825f6beaf72
Prerna <prernapsancheti@gmail.com>
2020-04-10 15:50:48 -0400

    Created django project and app

--- profiles/wsgi.py
+++ profiles/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for profile_project project.
+WSGI config for profiles project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profile_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profiles.settings')
 
 application = get_wsgi_application()
